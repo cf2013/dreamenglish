2d2d863029748554aa3f1e754aba49039a8a7692
cf <carlosfrancisco.rg@gmail.com>
2021-12-11 10:33:23 -0600

    intervencion

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,12 @@ var modal = document.getElementById("myModal");
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
+// Get the modal
+var modal = document.getElementById("myModal2");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn2");
+
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
