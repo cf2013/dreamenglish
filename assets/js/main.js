b817cf7622ac181beaeddf7cea38cddecc869d51
HannahGloria <eleonor.ragl@gmail.com>
2022-02-23 14:08:58 -0600

    nueva imagen cambios

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,7 @@ var btn2 = document.getElementById("myBtn2");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close2")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
@@ -27,10 +28,17 @@ btn2.onclick = function() {
 span.onclick = function() {
   modal.style.display = "none";
 }
-
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+window.onclick = function(event) {
+  if (event.target == modal2) {
     modal.style.display = "none";
   }
 }
